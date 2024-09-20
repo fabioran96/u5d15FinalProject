7a014584523a1f6770fccf5fad8134ff2d543eec
Fabio Ranocchiari <fabio.ranocchiari@gmail.com>
2024-09-20 09:29:11 +0200

    Initial commit

---
+++ src/main/java/fabioran/u5d15FinalProject/U5d15FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package fabioran.u5d15FinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d15FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d15FinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fabioran/u5d15FinalProject/U5d15FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package fabioran.u5d15FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d15FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
